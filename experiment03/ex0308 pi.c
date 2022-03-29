955e9d12ac821cebe6ab48cb3584aafde84a3320
Wangyxht <wangyx0001@qq.com>
2022-03-29 23:52:02 +0800

    南京航空航天大学C语言程序设计实验

--- experiment03/ex0308 pi.c
+++ experiment03/ex0308 pi.c
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 #include<stdio.h>
-
 #include<stdlib.h>
-
-
 
 
 int main (void)
@@ -12,14 +7,9 @@ int main (void)
 {
 
     double  generalTrem=1;
-
     double  result=0;
-
     double  pi;
-
     int  counter;
-
-
 
     for(counter=1 ; generalTrem>1e-8 ; counter++)
 
@@ -27,43 +17,28 @@ int main (void)
 
         generalTrem=1.0/(2.0*counter-1);
 
-       
-
         if(counter % 2 ==0)
 
         {
-
             result=result-generalTrem;
-
         }
 
         else
-
         {
-
             result=result+generalTrem;
-
         }
 
     }
-
-
 
     pi=4*result;
 
     printf("π的值为：%lf\n",pi);
 
 
-
     system("pause");
-
     return 0;
 
 
 
 
 }
-=======
->>>>>>> 08daf5e3f525c9b72e49bc6ee13aef0a00bca065
-=======
->>>>>>> 08daf5e3f525c9b72e49bc6ee13aef0a00bca065
